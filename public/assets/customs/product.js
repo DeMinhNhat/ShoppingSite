995aade6e6744db93781ba46d308b3cac4cc497b
Minh Nhat <mocking.key118@gmail.com>
2019-05-16 19:26:30 +0700

    code product

--- public/assets/customs/product.js
+++ public/assets/customs/product.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
 	$('#prev').on('click', function() {
 		var page = $(this).data('page');
-		var total = Math.floor($(this).data('total'));
 		var query = $(this).data('query');
 
 		if (+page > 1) {
@@ -10,14 +9,13 @@ $(document).ready(function() {
 			window.location.replace(link);
 		}
 	});
+	
 	$('#next').on('click', function() {
 		var page = $(this).data('page');
-		var total = Math.floor($(this).data('total'));
 		var query = $(this).data('query');
+		var total = Math.floor($(this).data('total'));
 
-		var page = $(this).data('page');
 		if (+page < +total) {
-			$('.prev').parent().removeClass("disabled");
 			page += 1;
 			var link = "?page=" + page + "&query=" + query;
 			window.location.replace(link);
